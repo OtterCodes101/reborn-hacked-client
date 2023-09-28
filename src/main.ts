bd62a8622043c15b93c8fc15df9ff24fa2c3fde5
OtterDev Youthinkimaddingmylastname <oliver.anderson4435@gmail.com>
2023-09-28 15:19:14 +0000

    add step

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { displayToChat } from "./chat"
 import { registerSpider } from "./spider"
+import { registerStep } from "./step"
 console.log("[REBORNHACKEDCLIENT] Loading client...")
 registerSpider()
+registerStep()
 console.log("[REBORNHACKEDCLIENT] Finished!")

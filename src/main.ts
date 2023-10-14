c16762da5407e992eb7f0f24ad67915602c1a828
OtterDev <144633084+OtterCodes102@users.noreply.github.com>
2023-10-14 14:48:22 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { displayToChat } from "./chat"
 import { registerSpider } from "./spider"
 import { registerStep } from "./step"
+import { registerRegen } from "./regen"
 console.log("[REBORNHACKEDCLIENT] Loading client...")
 registerSpider()
 registerStep()
+registerRegen()
 console.log("[REBORNHACKEDCLIENT] Finished!")
